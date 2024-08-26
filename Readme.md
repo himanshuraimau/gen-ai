50022800ccb41c9e09fb42abe79876b684d04f51
Himanshu Rai <himanshuraimau9@gmail.com>
2024-08-26 12:09:53 +0530

    add files

---
+++ Readme.md
@@ -0,0 +1,42 @@
+
+# Web Scraper Application
+
+## Overview
+
+This project is a web scraper application that allows you to scrape data from a provided URL and display the content. The application consists of a backend server built with Express and a frontend served as static HTML.
+
+## Getting Started
+
+To get the application up and running, follow these steps:
+
+1. **Clone the Repository**
+
+   ```bash
+   git clone https://github.com/himanshuraimau/gen-ai
+   cd gen-ai
+   ```
+
+2. **Setup Environment Variables**
+
+   - Rename the `.env.example` file to `.env`.
+   - Open the `.env` file and fill in the required environment variables.
+
+3. **Install Dependencies**
+
+   Run the following command to install the necessary dependencies:
+
+   ```bash
+   npm install
+   ```
+
+4. **Start the Application**
+
+   To start the development server, use:
+
+   ```bash
+   npm run dev
+   ```
+
+   The server will be running on port `3000` by default. You can access the frontend at `http://localhost:3000`.
+
+5. **The output json file is in the `question.json` folder.
--- scrapper.js
+++ scrapper.js
@@ -1,30 +1,36 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
 import axios from 'axios';
 import fs from 'fs';
 import url from 'url';
+import stealth from 'puppeteer-extra-plugin-stealth';
+import dotenv from 'dotenv';
 
+dotenv.config()
+puppeteer.use(stealth());
 let page_content = [];
 
 export const scrapper = async () => {
     
     const browser = await puppeteer.launch({ headless: true }); 
     const page = await browser.newPage();
-    const baseURL = "https://www.decoderssit.com/";
+    const baseURL = process.env.SCRAP_URL
     
-    await page.goto(baseURL, { waitUntil: 'networkidle0' });
+    
+    await page.goto(baseURL, { waitUntil: 'networkidle0', timeout: 40000 });
 
     const links = await page.evaluate(() => {
         return Array.from(document.querySelectorAll('a')).map(anchor => anchor.href).filter(href => href); // Filter out empty hrefs
     });
 
-    console.log(`Extracted ${links.length} links`);
     const uniqueLinks = new Set(links);
 
     const fetchContentPromises = Array.from(uniqueLinks).map(async (link) => {
         try{
-            const absoluteUrl = url.resolve(baseURL, link);
+            // const absoluteUrl = url.resolve(baseURL, link);
+            const absoluteUrl = "https://r.jina.ai/"+link
+            console.log(absoluteUrl);
             const response = await axios.get(absoluteUrl);
-            page_content.push({ "link": absoluteUrl, "content": response.data });
+            page_content.push({ "link": link, "content": response.data });
         } catch (error) {
             console.error(`Error fetching ${link}:`, error.message);
         }
